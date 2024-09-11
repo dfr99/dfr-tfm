7974a95eb33a01f6da60dc770fc74bcdd6fef92a
David Fraga Rodríguez <david.fraga.rodriguez@udc.es>
2024-09-11 12:51:54 +0200

    NextCloud ETL + validate Python workflow (#37)
    * WIP: NextCloud ETL
    
    * Change RDS DB engine from Postgres to MySQL
    
    * Validate SQS module
    
    * Add validate-python.yml
    
    * Add validate-python.yml
    
    * terraform-docs[bot]: update terraform docs
    
    Signed-off-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    
    * Update validate-python.yml
    
    Signed-off-by: David Fraga Rodríguez <david.fraga.rodriguez@udc.es>
    
    ---------
    
    Signed-off-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Signed-off-by: David Fraga Rodríguez <david.fraga.rodriguez@udc.es>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- code/terraform/nextcloud/terraform.tf
+++ code/terraform/nextcloud/terraform.tf
@@ -44,6 +44,6 @@ data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 
 locals {
-  account_id = data.aws_caller_identity.current.account_id
-  region     = data.aws_region.current.name
+  aws_account_id = data.aws_caller_identity.current.account_id
+  region         = data.aws_region.current.name
 }
